2961210f4230a06aefd88b5dbcfff0e88f572db6
SilverRid <41903077+SilverRid@users.noreply.github.com>
2022-01-25 12:26:25 +0300

    Init project

---
+++ src/main/java/ru/nposp/tech/frontclient/FrontclientApplication.java
@@ -0,0 +1,13 @@
+package ru.nposp.tech.frontclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nposp/tech/frontclient/FrontclientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nposp.tech.frontclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
